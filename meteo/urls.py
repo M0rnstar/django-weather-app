a7138cb78d662689a4f6617a9c06fdebcb940837
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-06-01 10:34:42 +0300

    route for autocomplete

--- meteo/urls.py
+++ meteo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view, stats_api
+from .views import home_view, stats_api, autocomplete_city
 
 urlpatterns = [
     path("home/", home_view, name="home"),
     path("api/stats/", stats_api, name="stats_api"),
+    path("autocomplete/", autocomplete_city, name="autocomplete_city"),
 ]
